def07ccd9f56ae0f490632c64271dd380bae0408
Camak2441 <ak2441@cam.ac.uk>
2024-03-08 12:17:09 +0000

    Further centralised button code

--- assets/scripts/windows/Placeholderdrag.gd
+++ assets/scripts/windows/Placeholderdrag.gd
@@ -1,14 +1,15 @@
 extends Sprite2D
-var dragging =false
+var dragging = false
 var of = Vector2 (0,0)
 # Called when the node enters the scene tree for the first time.
 
+func _ready():
+	visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if dragging:
 		position = get_global_mouse_position() - of
-
 
 func _on_button_button_down():
 	dragging = true # Replace with function body.
@@ -20,4 +21,5 @@ func _on_button_button_up():
 
 
 func _on_cone_button_pressed():
+	visible = !visible;
 	position = Vector2 (262,-306)  # Replace with function body.

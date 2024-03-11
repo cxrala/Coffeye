15d8cdc1a0dfb423c08b117b685b71b809c5436c
Camak2441 <ak2441@cam.ac.uk>
2024-03-11 21:02:53 +0000

    Prevented windows from leaving the desktop

--- assets/scripts/windows/Window.gd
+++ assets/scripts/windows/Window.gd
@@ -1,6 +1,10 @@
 extends Node2D
 var dragging = false
-var of = Vector2 (0,0)
+var off = Vector2 (0,0)
+@export var windowSize = Vector2 (176, 128)
+@export var windowTopLeftCorner = Vector2 (-88, -4)
+@export var desktopSize = Vector2 (352, 204)
+@export var desktopTopLeftCorner = Vector2 (16, 12)
 # Called when the node enters the scene tree for the first time.
 
 @export var bar : TextureButton
@@ -14,11 +18,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if dragging:
-		position = (0.2 * get_global_mouse_position()) - of
+		position = (0.2 * get_global_mouse_position()) - off
+	if position.x + windowTopLeftCorner.x < desktopTopLeftCorner.x :
+		position.x = desktopTopLeftCorner.x - windowTopLeftCorner.x
+	elif position.x + windowTopLeftCorner.x + windowSize.x > desktopTopLeftCorner.x + desktopSize.x :
+		position.x = desktopTopLeftCorner.x + desktopSize.x - windowTopLeftCorner.x - windowSize.x
+	if position.y + windowTopLeftCorner.y < desktopTopLeftCorner.y :
+		position.y = desktopTopLeftCorner.y - windowTopLeftCorner.y
+	elif position.y + windowTopLeftCorner.y + windowSize.y > desktopTopLeftCorner.y + desktopSize.y :
+		position.y = desktopTopLeftCorner.y + desktopSize.y - windowTopLeftCorner.y - windowSize.y
+	
 
 func _on_bar_down():
 	dragging = true
-	of = (0.2 * get_global_mouse_position()) - position
+	off = (0.2 * get_global_mouse_position()) - position
 
 func _on_bar_up():
 	dragging = false
